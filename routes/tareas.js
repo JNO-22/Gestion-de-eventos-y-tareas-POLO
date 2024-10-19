b0005a4948158ff1f0b258d094e6d7b0649c4ec6
Jebuslr <jebuslr.11@gmail.com>
2024-10-19 18:54:52 -0300

    feat: agregado 4 endpoint

--- routes/tareas.js
+++ routes/tareas.js
@@ -46,4 +46,54 @@ router.put('/editar/:id', async(req,res)=>{
     }
 })
 
+
+router.delete('/borrar/:id', async(req,res)=>{
+    try {
+        let {id} = req.params;
+        let respuesta = await modelTarea.findByIdAndDelete(id);        
+        if(respuesta != null){
+            res.status(200).send({ mensaje: "Tarea borrada exitosamente", body: respuesta })   
+        }else{
+            res.status(400).send({ mensaje: "Error al borrar una tarea", error: error });
+        }
+
+    } catch (error) {
+        res.status(400).send({ mensaje: "Error al borrar una tarea", error: error });
+    }
+})
+
+router.get('/:id', async(req,res)=>{
+    try {
+        let {id} = req.params;
+        let respuesta = await modelTarea.findById(id);
+        res.status(200).send({ mensaje: "Tarea encontrada exitosamente por id", body: respuesta }) 
+    } catch (error) {
+        res.status(400).send({ mensaje: "Error al buscar una tarea por id", error: error });
+    }  
+})
+
+router.put('/:id/completar', async(req,res)=>{
+    try {
+        let {id} = req.params;
+        let tareaVieja = await modelTarea.findByIdAndUpdate(id,{completada:true}, { new: true });
+        res.status(200).send({ mensaje: "Tarea completada exitosamente", body: tareaVieja }) 
+    } catch (error) {
+        res.status(400).send({ mensaje: "Error al completar la tarea", error: error });
+    }  
+})
+
+router.get('/prioridad/:nivel', async(req,res)=>{
+    try {
+        let {nivel} = req.params;
+        let tareas = await modelTarea.find({prioridad:nivel});
+        
+        if(tareas.length == 0){
+         return res.status(400).send({ mensaje: "no se encontraron tareas con esa prioridad", body: tareas});
+        } 
+        res.status(200).send({ mensaje: "Tareas con prioridad ", body: tareas });
+    } catch (error) {
+        res.status(400).send({ mensaje: "Error filtrar tareas", error: error });
+    }  
+})
+
 module.exports = router;
